c7dcdc74c6b082515c417606485ec3a0f35fe3ce
nunu2331 <yudhafin233@gmail.com>
2020-08-24 23:29:47 +0700

    belajar jsx rumtum

--- App.js
+++ App.js
@@ -1,32 +1,15 @@
 import  React from 'react'
-import { View, Text, Image, ScrollView, TextInput } from 'react-native'
+import { Text } from 'react-native'
 
-const App = () => {
-  return (
-    <ScrollView>
-      <Text>Ini teks</Text>
-      <View>
-        <Text>
-          some more teks
-        </Text>
-        <Image 
-        source={{
-          uri:
-          'https://reactnative.dev/docs/assets/p_cat2.png',
-        }}
-        style={{ width: 200, height: 200}}
-        />
-      </View>
-      <TextInput 
-        style={{
-          height: 400,
-          borderColor: "gray",
-          borderWidth: 1
-        }}
-        defaultValue="bisa diisi ini gan"
-      />
-    </ScrollView>
-  )
+const getFullName = (firstName, secondName, thirdName) => {
+  return firstName + "" + secondName + "" + thirdName
 }
 
-export default App
+const cat = () => {
+  return (
+    <Text>
+      Halo, Aku {getFullName("Rum", "Tum", "Tugger")}
+    </Text>
+  )
+}
+export default cat 
