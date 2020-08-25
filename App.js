67554e0a2668a034af84f89d36d4f9133bea6baa
nunu2331 <yudhafin233@gmail.com>
2020-08-25 21:27:42 +0700

    belajar listview2

--- App.js
+++ App.js
@@ -1,40 +1,41 @@
 import  React from "react"
-import { FlatList, StyleSheet, Text, View } from "react-native"
+import { FlatList, StyleSheet, Text, View, SectionList } from "react-native"
 
-const style = StyleSheet.create ({
+const styles = StyleSheet.create ({
   container: {
     flex: 1,
     paddingTop: 22
   },
+  sectionHeader: {
+    paddingTop:2,
+    paddingLeft:10,
+    paddingRight:10,
+    paddingBottom:2,
+    fontSize:14,
+    fontWeight:'bold',
+    backgroundColor:'rgba(274,247,247,1.0)',
+  },
   item: {
     padding: 10,
     fontSize: 18,
-    height: 44
-  }
+    height: 44,
+  },
 })
 
-const FlatListBasics = () => {
+const SectionListBasics = () => {
   return (
-    <View style={style.container}>
-      <FlatList
-        data={[
-          {key: 'Devin'},
-          {key: 'Aru'},
-          {key: 'Den'},
-          {key: 'Donn'},
-          {key: 'Dann'},
-          {key: 'Deon'},
-          {key: 'Duin'},
-          {key: 'Dionn'},
-          {key: 'Dras'},
-          {key: 'Dreno'},
-          {key: 'Drako'},
-          {key: 'Derko'},
-          {key: 'Draco'},
+    <View style={styles.container}>
+      <SectionList
+        sections={[
+          {title: 'A', data: ['Agus', 'Amri', 'AkuGanteng']},
+          {title: 'B', data: ['Bagus', 'Bamri', 'BakuGanteng']},
         ]}
-        renderItem={({item}) => <Text style={style.item}>{item.key}</Text>}
+        renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+        renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+        keyExtractor={(item, index) => index}
       />
     </View>
   )
 }
-export default FlatListBasics
+
+export default SectionListBasics
