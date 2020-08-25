a0811ccee7a3b6d82635bfdedb24978d23bc5dcd
nunu2331 <yudhafin233@gmail.com>
2020-08-25 21:47:56 +0700

    belajar style

--- App.js
+++ App.js
@@ -1,41 +1,29 @@
 import  React from "react"
 import { FlatList, StyleSheet, Text, View, SectionList } from "react-native"
 
-const styles = StyleSheet.create ({
-  container: {
-    flex: 1,
-    paddingTop: 22
-  },
-  sectionHeader: {
-    paddingTop:2,
-    paddingLeft:10,
-    paddingRight:10,
-    paddingBottom:2,
-    fontSize:14,
-    fontWeight:'bold',
-    backgroundColor:'rgba(274,247,247,1.0)',
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-})
-
-const SectionListBasics = () => {
+const LotsOfStyles = () => {
   return (
     <View style={styles.container}>
-      <SectionList
-        sections={[
-          {title: 'A', data: ['Agus', 'Amri', 'AkuGanteng']},
-          {title: 'B', data: ['Bagus', 'Bamri', 'BakuGanteng']},
-        ]}
-        renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-        renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-        keyExtractor={(item, index) => index}
-      />
+      <Text style={styles.red}>just red</Text>
+      <Text style={styles.bigBlue}>just bigBlue</Text>
+      <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+      <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
     </View>
   )
 }
 
-export default SectionListBasics
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  }
+})
+
+export default LotsOfStyles
