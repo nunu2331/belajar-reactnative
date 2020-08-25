0e8843a3f092f9509fa94ed34c510063695dd7a2
nunu2331 <yudhafin233@gmail.com>
2020-08-25 21:51:56 +0700

    fixed dimensions

--- App.js
+++ App.js
@@ -1,29 +1,14 @@
 import  React from "react"
 import { FlatList, StyleSheet, Text, View, SectionList } from "react-native"
 
-const LotsOfStyles = () => {
+const FixedDimensionBasics =() => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.red}>just red</Text>
-      <Text style={styles.bigBlue}>just bigBlue</Text>
-      <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
-      <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+    <View>
+      <View style= {{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+      <View style= {{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+      <View style= {{width: 500, height: 500, backgroundColor: 'steelblue'}} />
     </View>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 50,
-  },
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  }
-})
-
-export default LotsOfStyles
+export default FixedDimensionBasics
